25015ca99e4ec9c98573ecfe2fe043d6e018594e
Pavel Dubov <dubov94@gmail.com>
2016-12-15 08:06:38 +0300

    Add VueRouter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import Index from './views/Index.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Index }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router
 })
