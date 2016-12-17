47f17cd6697c6b0f4f52824956eacf66efb06f63
Pavel Dubov <dubov94@gmail.com>
2016-12-17 23:21:40 +0300

    Add cinema page text

--- src/views/Cinema.vue
+++ src/views/Cinema.vue
@@ -1,11 +1,28 @@
 <style lang="scss" scoped>
     @import '../assets/global.scss';
+
+    .double-text {
+        margin-top: $wrapper-offset;
+        text-align: center;
+    }
 </style>
 
 <template>
     <div class="container--center">
         <div class="wrapper">
             <c-header :title="['Кино', 'Cinema']" :oq="true"></c-header>
+            <div class="double-text">
+                <div class="double-text__black">
+                    &laquo;Бедный Пьеро&raquo;<br>
+                    &laquo;Клоун и его собачки&raquo;<br>
+                    &laquo;Кружка пива&raquo;
+                </div>
+                <div class="double-text__blue">
+                    Pauvre Pierrot<br>
+                    Clown et ses chiens<br>
+                    Un bon bock
+                </div>
+            </div>
         </div>
     </div>
 </template>
