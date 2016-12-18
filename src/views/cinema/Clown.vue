44bf3441296780ff4dda6c878c8ed2b7c0adedbf
Pavel Dubov <dubov94@gmail.com>
2016-12-18 21:00:58 +0300

    Add favicon

--- src/views/cinema/Clown.vue
+++ src/views/cinema/Clown.vue
@@ -10,7 +10,7 @@
         </div>
         <div class="paper paper--article">
             <div class="paper__half">
-<p>Немой короткометражный мультфильм режиссёра Эмиля Рейно. Фильм был показан вместе с мультфильмом «Кружка пива» в Музее Гревен. Премьера состоялась там же 28 октября 1892 года. Мультфильм утерян</p>
+<p>Немой короткометражный мультфильм режиссёра Эмиля Рейно. Фильм был показан вместе с мультфильмом «Кружка пива» в Музее Гревен. Премьера состоялась там же 28 октября 1892 года. Мультфильм утерян.</p>
             </div>
             <div class="paper__half">
 <p>It is an 1892 French short animated film directed by Émile Reynaud. It consists of 300 individually painted images and lasts about 15 minutes. It is one of the first animated films ever made – though largely forgotten, it represented a major advance in motion picture technology.
